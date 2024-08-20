94bafb450b73fa04bfa9d44d4d177baa8849417f
alejandrossm <alejandrossm@outlook.com>
2024-08-20 17:27:12 -0400

    rutas listas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AyudaComponent } from './pages/ayuda/ayuda.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'ayuda',
+    component:AyudaComponent
+  },
+  {
+    path:'contacto',
+    component:ContactoComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { AyudaComponent } from './pages/ayuda/ayuda.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactoComponent,
+    AyudaComponent
   ],
   imports: [
     BrowserModule,
